0c175f67934bb937cbe425cb71d3709244f78566
Grégoire Pineau <lyrixx@lyrixx.info>
2013-08-13 17:39:03 +0200

    Updated color set and hash color algo

--- src/Lyrixx/Bundle/FortuneBundle/Twig/FortuneExtension.php
+++ src/Lyrixx/Bundle/FortuneBundle/Twig/FortuneExtension.php
@@ -5,13 +5,20 @@ namespace Lyrixx\Bundle\FortuneBundle\Twig;
 class FortuneExtension extends \Twig_Extension
 {
     private static $defaultColors = array(
-        '#FF0000',
-        '#FF7F00',
-        '#FFFF00',
-        '#00FF00',
-        '#0000FF',
-        '#4B0082',
-        '#8F00FF',
+        0 => '#fa3c3c',
+        1 => '#00dc00',
+        2 => '#1e3cff',
+        3 => '#00c8c8',
+        4 => '#f00082',
+        5 => '#e6dc32',
+        6 => '#f08228',
+        7 => '#a000c8',
+        8 => '#a0e632',
+        9 => '#00a0ff',
+        10 => '#e6af2d',
+        11 => '#00d28c',
+        12 => '#8200dc',
+        13 => '#aaaaaa',
     );
 
     private $ring;
@@ -37,6 +44,16 @@ class FortuneExtension extends \Twig_Extension
 
     public function randomColor($text)
     {
+        $textRaw = $text;
+        // We want to normalize the name
+        $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+        $text = trim($text, '-');
+        if (function_exists('iconv')) {
+            $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+        }
+        $text = strtolower($text);
+        $text = preg_replace('~[^-\w]+~', '', $text);
+
         // max is 255
         $hash = hexdec(substr(sha1($text), 0, 2));
 
@@ -46,7 +63,7 @@ class FortuneExtension extends \Twig_Extension
             }
         }
 
-        return sprintf('<span style="color:%s">%s</span>', $color, $text);
+        return sprintf('<span style="color:%s">%s</span>', $color, $textRaw);
     }
 
     public function getName()
